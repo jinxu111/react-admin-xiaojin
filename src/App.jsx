723c2f0a914597655c589a6c3ac4093de1672465
xiaojin <1499411376@qq.com>
2019-11-19 13:15:44 +0800

    update peizhi

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import {Button} from 'antd';
+export default class App extends Component{
+    render(){
+        return (
+            <div>
+                <Button type="primary"></Button>
+            </div>
+        )
+    }
+}
